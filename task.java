049600511422d11dfdd4ba677ee2d1f663d4764c
Dharanikala <dharanidharu011@gmail.com>
2018-05-30 12:39:31 +0530

    hi

---
+++ task.java
@@ -0,0 +1,17 @@
+package tasks;
+
+public class task {
+	public static void main(String[] args) {
+		int count = 10;
+		
+		if(count < 20) {
+			System.out.println(20);
+		}
+		else{
+			System.out.println(10);
+		}
+			
+		}
+	}
+
+
