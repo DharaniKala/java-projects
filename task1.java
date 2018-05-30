a28b7c74eba7c99cbc2f4f42385b8fb0bb055f0a
Dharanikala <dharanidharu011@gmail.com>
2018-05-30 12:53:04 +0530

    hai

---
+++ task1.java
@@ -0,0 +1,14 @@
+package tasks;
+
+public class task1 {
+  public static void main (String S[]) {
+	  int count = 10;
+	  if(count < 20) {
+			System.out.println(20);
+		}
+		else{
+			System.out.println(10);
+		}
+			int response = count > 20 ? 100 : 20;
+  }
+}
