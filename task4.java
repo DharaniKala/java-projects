58f8a6bf90a700b9abdc26b8c9c54d6f1542fe9c
Dharanikala <dharanidharu011@gmail.com>
2018-05-31 10:13:45 +0530

    label

---
+++ task4.java
@@ -0,0 +1,18 @@
+package tasks;
+
+public class task4 {
+public static void main(String S[]) {
+	outer : for (int j=0; j<5;j++) {
+		inner : for(int k=0; k<3; k++) {
+			if(k==1) 
+        continue outer;
+                if(k==2)
+                	continue inner;
+            	System.out.println("j = " +j+ " k = " +k);           
+	
+			
+		}
+		
+	}
+}
+}
