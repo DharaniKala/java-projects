f5826dc3a0f11c0a27ee6b23c445ebb49afeb891
Dharanikala <dharanidharu011@gmail.com>
2018-06-01 16:34:07 +0530

    array

---
+++ Array.java
@@ -0,0 +1,29 @@
+package tasks;
+
+public class Array {
+	public static void main (String []args) {
+		
+		int squares []  =  new int [4];
+		
+	      for(int i = 0; i< squares.length; i++) 
+	      {
+	    	  squares[i] = i * 10;         
+	      }
+	      
+	      //squares [4] = 30;
+	      
+	       int data []= {2,3,1,2,1,4};
+	       
+	       
+	      Car car [] = {new Car(), new Car(), new Car ()};
+	      
+	      
+	      for(int i = 0; i< args.length; i++)
+	      {
+	    	  System.out.println(args[i]);
+	      }
+	      
+	}
+	}
+
+
