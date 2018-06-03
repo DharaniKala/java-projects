da76a3c6bc091022d3c5a10355bf5be564dc608a
Dharanikala <dharanidharu011@gmail.com>
2018-06-03 16:59:19 +0530

    StudentId

--- Student.java
+++ Student.java
@@ -1,50 +1,51 @@
-package com.java;
+package student1;
 
 public class Student {
 	private String studentId;
-private String firstName;
-private String lastName;
-private String gender;
-private String branch;
-private String department;
-public String getStudentId() {
-	return studentId;
-}
-public void setStudentId(String studentId) {
-	this.studentId = studentId;
-}
-public String getFirstName() {
-	return firstName;
-}
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
-}
-public String getLastName() {
-	return lastName;
-}
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-}
-public String getGender() {
-	return gender;
-}
-public void setGender(String gender) {
-	this.gender = gender;
-}
-public String getBranch() {
-	return branch;
-}
-public void setBranch(String branch) {
-	this.branch = branch;
-}
-public String getDepartment() {
-	return department;
-}
-public void setDepartment(String department) {
-	this.department = department;
-}
-@Override
-public String toString() {
-	return studentId+" "+firstName+" "+lastName+" "+gender+" "+branch+" "+department;
-}
-}
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String branch;
+	private String department;
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	@Override
+	public String toString() {
+		return studentId+" "+firstName+" "+lastName+" "+gender+" "+branch+" "+department;
+	}
+	}
+
